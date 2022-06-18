cb5107fa52dae9d6eb99ca32788f17b5c200e0ec
vamsi kalagaturu <vamsikalagaturu@gmail.com>
2022-06-18 15:54:55 +0200

    bug fix and sbatch partition to gpu with 100G

--- sh_files/clear_sky_filter.sh
+++ sh_files/clear_sky_filter.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 
-#SBATCH --partition=wr14         # partition (queue)
+#SBATCH --partition=gpu         # partition (queue)
 #SBATCH --tasks=32               # number of tasks per node
-#SBATCH --mem=4G                # memory per node in MB <180G> (different units with suffix K|M|G|T)
+#SBATCH --mem=18G                # memory per node in MB <180G> (different units with suffix K|M|G|T)
 #SBATCH --time=12:00:00           # total runtime of job allocation (format D-HH:MM:SS; first parts optional)
 #SBATCH --output=slurm.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
 #SBATCH --error=slurm.%j.err     # filename for STDERR

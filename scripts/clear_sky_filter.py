76d1da963f262d1cad129a295016ac7154c34848
vamsi kalagaturu <vamsikalagaturu@gmail.com>
2022-06-18 16:35:50 +0200

    more bug fixes

--- scripts/clear_sky_filter.py
+++ scripts/clear_sky_filter.py
@@ -108,8 +108,10 @@ for j in range(len(total_days)):
         (cs_bool*500).plot(ax=ax)
         plt.savefig('/scratch/vkalag2s/ASIRE-code-base/clear_sky_images/'+total_days[j].strftime("%Y-%m-%d")+'.png')
         plt.close()
-clear_sky_bool_df['file_paths'] = image_data[0]
-clear_sky_bool_df['ghi'] = rad_data['ghi']
+clear_sky_bool_df['file_paths'] = image_data[0].to_numpy()
+clear_sky_bool_df['ghi'] = rad_data['ghi'].to_numpy()
+
+clear_sky_bool_df.to_csv('/scratch/vkalag2s/ASIRE-code-base/clear_sky_bool_df.csv')
 
 clear_sky_true = clear_sky_bool_df[clear_sky_bool_df['clear_sky'] == "true"]
 clear_sky_false = clear_sky_bool_df[clear_sky_bool_df['clear_sky'] == "false"]
